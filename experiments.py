c439f8f1a3931a4786694c9323b2fbe65f113c81
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2018-07-08 16:38:27 +1200

    Add regularisation

--- experiments.py
+++ experiments.py
@@ -3,24 +3,25 @@ import re
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from cycler import cycler
+
 import ml_dataset_loader.datasets as data_loader
 
 test_size = 0.2
 train_size = 0.6
 validation_size = 0.2
 random_seed = 7
-num_rounds = 1000
+num_rounds = 20000
+early_stopping_rounds = 50
 
-common_param = {'eta': 0.05, 'silent': 1, 'tree_method':'gpu_hist'}
+common_param = {'eta': 0.05, 'silent': 1, 'tree_method': 'gpu_hist', 'subsample': 0.9,
+                'colsample_bytree': 0.7}
 optimizer_param = [
     {'name': 'Baseline', 'optimizer': 'default_optimizer'},
-                   {'name': 'Momentum', 'optimizer': 'momentum_optimizer', 'momentum': 0.3},
+    {'name': 'Momentum', 'optimizer': 'momentum_optimizer', 'momentum': 0.3},
     {'name': 'Nesterov', 'optimizer': 'nesterov_optimizer', 'momentum': 0.3},
-                   ]
+]
 
 
 class Experiment:
@@ -55,17 +56,17 @@ class Experiment:
                 param["num_class"] = np.max(y_train) + 1
             if self.objective == "binary:logistic":
                 pos = np.sum(y_train)
-                param["scale_pos_weight"] = (len(y_train) - pos)/pos
+                param["scale_pos_weight"] = (len(y_train) - pos) / pos
                 print(param["scale_pos_weight"])
 
             res = {}
             bst = xgb.train(param, dtrain, num_rounds,
                             evals=[(dtrain, "train"), (dtest, "test"), (dval, "val")],
-                            early_stopping_rounds=5, evals_result=res)
-            data[param["name"]] = res['test'][self.metric][:bst.best_iteration+1]
+                            early_stopping_rounds=early_stopping_rounds, evals_result=res)
+            data[param["name"]] = res['test'][self.metric][:bst.best_iteration + 1]
             # Plot series
-            plt.plot(data[param["name"]], marker='x',  markevery = [bst.best_iteration], label=param["name"])
-
+            plt.plot(data[param["name"]], marker='x', markevery=[bst.best_iteration],
+                     label=param["name"])
 
         # Plot
         plt.xlabel('iterations')
@@ -73,14 +74,14 @@ class Experiment:
         plt.legend()
         plt.title(self.name)
         snake_name = re.sub(' ', '_', self.name).lower()
-        plt.savefig('figures/' + snake_name + '.pgf',bbox_inches='tight')
-        plt.savefig('figures/' + snake_name + '.png',bbox_inches='tight')
+        plt.savefig('figures/' + snake_name + '.pgf', bbox_inches='tight')
+        plt.savefig('figures/' + snake_name + '.png', bbox_inches='tight')
 
         # Zoom
         plt.xlim(1e15, 0)
         for name, series in data.items():
             xmin, xmax = plt.xlim()
-            plt.xlim(min(xmin, len(series)- 1), max(xmax,len(series)-1))
+            plt.xlim(min(xmin, len(series) - 1), max(xmax, len(series) - 1))
 
         plt.ylim(1e15, 0)
         for name, series in data.items():
@@ -90,24 +91,24 @@ class Experiment:
         # Add padding
         padding = 0.35
         xmin, xmax = plt.xlim()
-        xpad = abs(xmin - xmax)*padding
-        plt.xlim(xmin-xpad,xmax + xpad)
+        xpad = abs(xmin - xmax) * padding
+        plt.xlim(xmin - xpad, xmax + xpad)
         ymin, ymax = plt.ylim()
-        ypad = abs(ymin - ymax)*padding
-        plt.ylim(ymin-ypad,ymax + ypad)
+        ypad = abs(ymin - ymax) * padding
+        plt.ylim(ymin - ypad, ymax + ypad)
 
-        plt.savefig('figures/' + snake_name + '_zoomed.pgf',bbox_inches='tight')
-        plt.savefig('figures/' + snake_name + '_zoomed.png',bbox_inches='tight')
+        plt.savefig('figures/' + snake_name + '_zoomed.pgf', bbox_inches='tight')
+        plt.savefig('figures/' + snake_name + '_zoomed.png', bbox_inches='tight')
 
 
-num_rows = 10000
+num_rows = 500000
 experiments = [
     Experiment("Synthetic regression", "reg:linear", "rmse", data_loader.get_synthetic_regression),
     Experiment("Cover type", "multi:softmax", "merror", data_loader.get_cover_type),
-    Experiment("YearPredictMSD",  "reg:linear", "rmse", data_loader.get_year),
-    Experiment("Higgs",  "binary:logistic", "error", data_loader.get_higgs),
-    Experiment("Bosch",  "binary:logistic", "auc", data_loader.get_bosch),
-    Experiment("Airline",  "binary:logistic", "error", data_loader.get_airline),
+    Experiment("YearPredictMSD", "reg:linear", "rmse", data_loader.get_year),
+    Experiment("Higgs", "binary:logistic", "error", data_loader.get_higgs),
+    Experiment("Bosch", "binary:logistic", "auc", data_loader.get_bosch),
+    Experiment("Airline", "binary:logistic", "error", data_loader.get_airline),
 ]
 
 matplotlib.use('pgf')
